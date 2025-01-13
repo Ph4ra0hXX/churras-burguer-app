8c7ca6b635088ad51389c5daa8a0812a739662c1
Ph4ra0hXX <ph4ra0hx@gmail.com>
2025-01-13 13:01:28 -0300

    teste

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="rodape">
     <br />
-    Quarta a segunda, das 18:30h as 23:00h<br />
+    Quarta a Domingo, das 18:30h as 23:00h<br />
     <br />
     José Ferreira Sombra, 1935, apto 101, Luís Alves de Freitas <br />
     Em cima da Loja LugaSom<br />
     <br />
-    <a href="https://www.instagram.com/churrasburguer_ln
-/?hl=pt-br" class="fa fa-lg fa-instagram"></a>
-    <a href="https://www.instagram.com/churrasburguer_ln
-/?hl=pt-br">@churrasburguer_ln
+    <a
+      href="https://www.instagram.com/churrasburguer_ln
+/?hl=pt-br"
+      class="fa fa-lg fa-instagram"
+    ></a>
+    <a
+      href="https://www.instagram.com/churrasburguer_ln
+/?hl=pt-br"
+      >@churrasburguer_ln
     </a>
     <br />
     <br />
-    <a href="https://api.whatsapp.com/send?phone=5588921508522" class="fab fa-whatsapp"></a>
-    <a href="https://api.whatsapp.com/send?phone=5588921508522">(88) 9 2150-8522</a>
+    <a
+      href="https://api.whatsapp.com/send?phone=5588921508522"
+      class="fab fa-whatsapp"
+    ></a>
+    <a href="https://api.whatsapp.com/send?phone=5588921508522"
+      >(88) 9 2150-8522</a
+    >
     <br />
     <br />
     Criado e desenvolvido por
